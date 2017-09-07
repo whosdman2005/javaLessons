3fff68ab8d3b645a6500dc37d1d457fe2ccfa41f
hob <whosdman2005@gmail.com>
2017-09-06 23:18:04 -0700

    Comments lesson

---
+++ src/Comments.java
@@ -0,0 +1,35 @@
+/**
+ * This class shows the examples of comments in java code
+ * There are two types of comments in java: javadoc and developer comments
+ * 
+ * @author javaman
+ * @version 1.0.0
+ */
+
+
+public class Comments {
+	/**
+	 * This is the entry point when this class is executed
+	 * This prints numbers from 1 to 5
+	 * 
+	 * This type of comment is a JAVADOC
+	 * @param args String array arguments
+	 * 
+	 */
+	public static void main(String [] args) {
+		/*
+		 * These statements print numbers
+		 * from 1 to 5 on the same line
+		 * I have used System.out.print() statement
+		 * to print to the console
+		 */
+		System.out.print(1);
+		System.out.print(2);
+		System.out.print(3);
+		System.out.print(4);
+		System.out.print(5);
+		
+	}
+	
+
+}
