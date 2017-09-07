6f626a075d187f5ee1341a9e105a6ffe6183a11e
hob <whosdman2005@gmail.com>
2017-09-06 23:32:54 -0700

    added different kinds of comments tags

--- src/Comments.java
+++ src/Comments.java
@@ -17,17 +17,20 @@ public class Comments {
 	 * 
 	 */
 	public static void main(String [] args) {
+		//TODO: XYZ
 		/*
 		 * These statements print numbers
 		 * from 1 to 5 on the same line
 		 * I have used System.out.print() statement
 		 * to print to the console
 		 */
-		System.out.print(1);
-		System.out.print(2);
+		System.out.print(1); // Prints 1 to the console
+		System.out.print(2); /* Prints 2 to the console */
 		System.out.print(3);
 		System.out.print(4);
 		System.out.print(5);
+		
+		//TODO change the code to use loops
 		
 	}
 	
