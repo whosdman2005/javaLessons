e088ec2e8c8f0670235540421b90c144cfb076d8
hob <whosdman2005@gmail.com>
2017-09-10 22:09:45 -0700

    variable lesson

---
+++ workspace/LiteralsVariable/src/Variables.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ * Class to show examples of variables
+ * @author G74S
+ *
+ */
+public class Variables {
+	public static void main(String[] args) {
+		
+		int x = 100; //declaration & initialization on the same line
+		char var; //only declaration, no initialization
+		var = 'A'; //initializing var / assigning value to var
+		String name = "javaman"; // reference type declaration & initialization
+		
+				
+		System.out.println("The value of x: "+ x);
+		System.out.println("The value of var: "+ var);
+		System.out.println("My name is "+ name);
+		
+		
+		
+	}
+
+}
