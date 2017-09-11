9c4e90c2dabc0b1fe6af2716490af40eddef971a
hob <whosdman2005@gmail.com>
2017-09-10 19:54:41 -0700

    adding literals

---
+++ workspace/LiteralsVariable/src/IntegerLiterals.java
@@ -0,0 +1,14 @@
+/**
+ * Class to show examples of Integer literals
+ * 
+ * @author javaman
+ *
+ */
+public class IntegerLiterals {
+	public static void main(String[] args) {
+		System.out.println(15); //decimal
+		System.out.println(017); //octal
+		System.out.println(0xf); //hexadecimal
+		System.out.println(0B1111); //binary
+	}
+}
