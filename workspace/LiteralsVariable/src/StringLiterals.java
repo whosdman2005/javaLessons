aca953034b92f1443fd0c10981d52944c3278667
hob <whosdman2005@gmail.com>
2017-09-10 21:43:41 -0700

    String Literals lesson

---
+++ workspace/LiteralsVariable/src/StringLiterals.java
@@ -0,0 +1,13 @@
+
+public class StringLiterals {
+	public static void main(String[] args) {
+		System.out.println("");
+		System.out.println(" ");
+		System.out.println("A");
+		System.out.println("Java");
+		System.out.println("Hello\n ");
+		System.out.println("\"");
+		
+	}
+
+}
