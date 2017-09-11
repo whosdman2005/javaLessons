19bf0d756d1ae4085596cf76161f0e4a26319889
hob <whosdman2005@gmail.com>
2017-09-10 21:10:35 -0700

    added new lessons about different kinds of literals

---
+++ workspace/LiteralsVariable/src/CharacterLiterals.java
@@ -0,0 +1,35 @@
+
+public class CharacterLiterals {
+	public static void main(String[] args) {
+		System.out.println('a');
+		
+		System.out.print("->");
+		System.out.print('\n');
+		System.out.println("<-");
+		
+		System.out.print("->");
+		System.out.print('\t');
+		System.out.println("<-");
+		
+		System.out.print("->");
+		System.out.print('\\');
+		System.out.println("<-");
+		
+		System.out.print("->");
+		System.out.print('\'');
+		System.out.println("<-");
+		
+		System.out.println('\u0041'); // to get the unicode go to https://unicode-table.com/en, click the desired character to get the unicode value    u0041 -A
+		System.out.println(); // click Control Character & look for DEVANAGARI
+		System.out.println();
+		System.out.println();
+		
+		
+		
+		
+		
+	}
+	
+	
+
+}
---
+++ workspace/LiteralsVariable/src/FloatingPointLiterals.java
@@ -0,0 +1,13 @@
+
+public class FloatingPointLiterals {
+	public static void main(String [] args) {
+		System.out.println(74589787.12); //Standard
+		System.out.println(7.458978712E7); //Scientific
+		
+		System.out.println(0.314567821); //Standard
+		System.out.println(3.14567821E-1); //Scientific
+		
+		
+	}
+
+}
