a251bb6e748721418405b8f9d9a25bc453707e56
hob <whosdman2005@gmail.com>
2017-09-16 21:31:34 -0700

    Operators lesson

---
+++ workspace/Operators/src/_11AssignmentOperators.java
@@ -0,0 +1,32 @@
+/**
+ * 
+ * Class to show examples of Assignment operators
+ * 
+ * @author G74S
+ *
+ */
+public class _11AssignmentOperators {
+	public static void main(String [] args) {
+		int i1 = 100;
+		System.out.println(i1);
+		
+		i1 += 1; //i1 = i1 + 1 = 100 + 1 = 101
+		System.out.println(i1);
+		
+		i1 -= 2; //i1 = i1 -2 = 101 -2 = 99
+		System.out.println(i1);
+		
+		i1 *= 3; //i1 = i1 * 3 = 99 * 3 = 297
+		System.out.println(i1);
+		
+		i1 /= 4; // i1 = i1 /4 = 297 / 4 = 74
+		System.out.println(i1);
+		
+		i1 %= 5; //i1 = i1 % 5 = 74 % 5 = 4
+		System.out.println(i1);
+		
+		
+		
+	}
+
+}
